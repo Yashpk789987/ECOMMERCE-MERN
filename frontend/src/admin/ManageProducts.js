bbe50530867849c0e52ed68f563bbfb6026a9f9c
yashpk789987 <yashpk789987@gmail.com>
2020-04-08 04:55:10 +0530

    Update Product Updated(Admin) (Material UI)

--- frontend/src/admin/ManageProducts.js
+++ frontend/src/admin/ManageProducts.js
@@ -17,7 +17,6 @@ const ManageProducts = ({ history }) => {
       if (data.error) {
         console.log(data.error);
       } else {
-        console.log(data);
         setProducts(data);
       }
     });
@@ -47,25 +46,6 @@ const ManageProducts = ({ history }) => {
         <div className="col-12">
           <h2 className="text-center">Total {products.length} products</h2>
           <hr />
-          {/* <ul className="list-group">
-            {products.map((p, i) => (
-              <li
-                key={i}
-                className="list-group-item d-flex justify-content-between align-items-center"
-              >
-                <strong>{p.name}</strong>
-                <Link to={`/admin/product/update/${p._id}`}>
-                  <span className="badge badge-warning badge-pill">Update</span>
-                </Link>
-                <span
-                  onClick={() => destroy(p._id)}
-                  className="badge badge-danger badge-pill"
-                >
-                  Delete
-                </span>
-              </li>
-            ))}
-          </ul> */}
           <MaterialTable
             style={{ padding: "3%" }}
             title="All Products"
--- frontend/src/admin/UpdateProduct.js
+++ frontend/src/admin/UpdateProduct.js
@@ -1,205 +1,377 @@
-import React, { useState, useEffect } from 'react';
-import Layout from '../core/Layout';
-import { isAuthenticated } from '../auth';
-import { Link, Redirect } from 'react-router-dom';
-import { getProduct, getCategories, updateProduct } from './apiAdmin';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-const UpdateProduct = ({ match }) => {
-    const [values, setValues] = useState({
-        name: '',
-        description: '',
-        price: '',
-        categories: [],
-        category: '',
-        shipping: '',
-        quantity: '',
-        photo: '',
-        loading: false,
-        error: false,
-        createdProduct: '',
-        redirectToProfile: false,
-        formData: ''
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert from "@material-ui/lab/Alert";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
+import FormControl from "@material-ui/core/FormControl";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
+import Backdrop from "@material-ui/core/Backdrop";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { makeStyles } from "@material-ui/core/styles";
+
+import { DropzoneArea } from "material-ui-dropzone";
+
+import { API } from "../config";
+import AdminDashboard from "../user/AdminDashboard";
+import { isAuthenticated } from "../auth";
+import { getProduct, getCategories, updateProduct } from "./apiAdmin";
+
+const useStyles = makeStyles((theme) => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: "#fff",
+  },
+}));
+
+const UpdateProduct = ({ match, history }) => {
+  const classes = useStyles();
+  const [values, setValues] = useState({
+    name: "",
+    description: "",
+    price: "",
+    categories: [],
+    category: "",
+    shipping: "",
+    quantity: "",
+    photo: "",
+    loading: false,
+    error: "",
+    createdProduct: "",
+    redirectToProfile: false,
+    formData: "",
+  });
+  const [categories, setCategories] = useState([]);
+
+  const { user, token } = isAuthenticated();
+  const {
+    name,
+    description,
+    price,
+    category,
+    shipping,
+    quantity,
+    loading,
+    error,
+    createdProduct,
+    redirectToProfile,
+    formData,
+    photo,
+  } = values;
+
+  const init = (productId) => {
+    getProduct(productId).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        // populate the state
+        setValues({
+          ...values,
+          name: data.name,
+          description: data.description,
+          price: data.price,
+          category: data.category._id,
+          shipping: data.shipping ? "1" : "0",
+          quantity: data.quantity,
+          photo: `${API}/product/photo/${data._id}`,
+          formData: new FormData(),
+        });
+        // load categories
+        initCategories();
+      }
     });
-    const [categories, setCategories] = useState([]);
+  };
 
-    const { user, token } = isAuthenticated();
-    const {
-        name,
-        description,
-        price,
-        // categories,
-        category,
-        shipping,
-        quantity,
-        loading,
-        error,
-        createdProduct,
-        redirectToProfile,
-        formData
-    } = values;
+  // load categories and set form data
+  const initCategories = () => {
+    getCategories().then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        setCategories(data);
+      }
+    });
+  };
 
-    const init = productId => {
-        getProduct(productId).then(data => {
-            if (data.error) {
-                setValues({ ...values, error: data.error });
-            } else {
-                // populate the state
-                setValues({
-                    ...values,
-                    name: data.name,
-                    description: data.description,
-                    price: data.price,
-                    category: data.category._id,
-                    shipping: data.shipping,
-                    quantity: data.quantity,
-                    formData: new FormData()
-                });
-                // load categories
-                initCategories();
-            }
-        });
-    };
+  useEffect(() => {
+    init(match.params.productId);
+  }, []);
 
-    // load categories and set form data
-    const initCategories = () => {
-        getCategories().then(data => {
-            if (data.error) {
-                setValues({ ...values, error: data.error });
-            } else {
-                setCategories(data);
-            }
-        });
-    };
+  const handleChange = (name) => (event) => {
+    const value = name === "photo" ? event.target.files[0] : event.target.value;
 
-    useEffect(() => {
-        init(match.params.productId);
-    }, []);
+    formData.set(name, value);
+    setValues({ ...values, [name]: value });
+  };
 
-    const handleChange = name => event => {
-        const value = name === 'photo' ? event.target.files[0] : event.target.value;
-        formData.set(name, value);
-        setValues({ ...values, [name]: value });
-    };
+  const handleImage = (name) => (file) => {
+    const value = file[0];
+    formData.set(name, value);
+    setValues({ ...values, [name]: value });
+  };
 
-    const clickSubmit = event => {
-        event.preventDefault();
-        setValues({ ...values, error: '', loading: true });
+  const clickSubmit = (event) => {
+    event.preventDefault();
+    setValues({ ...values, error: "", loading: true });
 
-        updateProduct(match.params.productId, user._id, token, formData).then(data => {
-            if (data.error) {
-                setValues({ ...values, error: data.error });
-            } else {
-                setValues({
-                    ...values,
-                    name: '',
-                    description: '',
-                    photo: '',
-                    price: '',
-                    quantity: '',
-                    loading: false,
-                    error: false,
-                    redirectToProfile: true,
-                    createdProduct: data.name
-                });
-            }
-        });
-    };
-
-    const newPostForm = () => (
-        <form className="mb-3" onSubmit={clickSubmit}>
-            <h4>Post Photo</h4>
-            <div className="form-group">
-                <label className="btn btn-secondary">
-                    <input onChange={handleChange('photo')} type="file" name="photo" accept="image/*" />
-                </label>
-            </div>
-
-            <div className="form-group">
-                <label className="text-muted">Name</label>
-                <input onChange={handleChange('name')} type="text" className="form-control" value={name} />
-            </div>
-
-            <div className="form-group">
-                <label className="text-muted">Description</label>
-                <textarea onChange={handleChange('description')} className="form-control" value={description} />
-            </div>
-
-            <div className="form-group">
-                <label className="text-muted">Price</label>
-                <input onChange={handleChange('price')} type="number" className="form-control" value={price} />
-            </div>
-
-            <div className="form-group">
-                <label className="text-muted">Category</label>
-                <select onChange={handleChange('category')} className="form-control">
-                    <option>Please select</option>
-                    {categories &&
-                        categories.map((c, i) => (
-                            <option key={i} value={c._id}>
-                                {c.name}
-                            </option>
-                        ))}
-                </select>
-            </div>
-
-            <div className="form-group">
-                <label className="text-muted">Shipping</label>
-                <select onChange={handleChange('shipping')} className="form-control">
-                    <option>Please select</option>
-                    <option value="0">No</option>
-                    <option value="1">Yes</option>
-                </select>
-            </div>
-
-            <div className="form-group">
-                <label className="text-muted">Quantity</label>
-                <input onChange={handleChange('quantity')} type="number" className="form-control" value={quantity} />
-            </div>
-
-            <button className="btn btn-outline-primary">Update Product</button>
-        </form>
-    );
-
-    const showError = () => (
-        <div className="alert alert-danger" style={{ display: error ? '' : 'none' }}>
-            {error}
-        </div>
-    );
-
-    const showSuccess = () => (
-        <div className="alert alert-info" style={{ display: createdProduct ? '' : 'none' }}>
-            <h2>{`${createdProduct}`} is updated!</h2>
-        </div>
-    );
-
-    const showLoading = () =>
-        loading && (
-            <div className="alert alert-success">
-                <h2>Loading...</h2>
-            </div>
-        );
-
-    const redirectUser = () => {
-        if (redirectToProfile) {
-            if (!error) {
-                return <Redirect to="/" />;
-            }
+    updateProduct(match.params.productId, user._id, token, formData).then(
+      async (data) => {
+        if (data.error) {
+          setValues({ ...values, error: data.error });
+        } else {
+          await setValues({
+            ...values,
+            name: "",
+            description: "",
+            photo: "",
+            price: "",
+            quantity: "",
+            loading: false,
+            error: "",
+            redirectToProfile: true,
+            createdProduct: data.name,
+          });
+          history.goBack();
         }
-    };
-
-    return (
-        <Layout title="Add a new product" description={`G'day ${user.name}, ready to add a new product?`}>
-            <div className="row">
-                <div className="col-md-8 offset-md-2">
-                    {showLoading()}
-                    {showSuccess()}
-                    {showError()}
-                    {newPostForm()}
-                    {redirectUser()}
-                </div>
-            </div>
-        </Layout>
+      }
     );
+  };
+
+  const newProductForm = () => {
+    return (
+      <Paper style={{ marginLeft: "-25%", width: "150%" }} elevation={3}>
+        <Grid container>
+          <Grid
+            item
+            xs
+            style={{ padding: "3%", paddingTop: "1%", paddingBottom: "1%" }}
+          >
+            <TextField
+              variant="outlined"
+              style={{ width: "100%", marginBottom: "5%", marginTop: "5%" }}
+              value={name}
+              onChange={handleChange("name")}
+              type="text"
+              label="Name"
+            />
+            <TextField
+              variant="outlined"
+              style={{ width: "100%", marginBottom: "5%", marginTop: "5%" }}
+              value={price}
+              onChange={handleChange("price")}
+              type="number"
+              label="Price"
+            />
+            <FormControl
+              variant="outlined"
+              style={{ width: "100%", marginBottom: "5%", marginTop: "5%" }}
+            >
+              <InputLabel id="demo-simple-select-outlined-label">
+                Category
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-outlined-label"
+                id="demo-simple-select-outlined"
+                value={category}
+                onChange={handleChange("category")}
+                label="Category"
+              >
+                <MenuItem disabled={true}>Select Category</MenuItem>
+                {categories &&
+                  categories.map((c, i) => (
+                    <MenuItem key={i} value={c._id}>
+                      {c.name}
+                    </MenuItem>
+                  ))}
+              </Select>
+            </FormControl>
+            <FormControl
+              variant="outlined"
+              style={{ width: "100%", marginBottom: "5%", marginTop: "5%" }}
+            >
+              <InputLabel id="demo-simple-select-outlined-label">
+                Shipping
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-outlined-label"
+                id="demo-simple-select-outlined"
+                value={shipping}
+                onChange={handleChange("shipping")}
+                label="Category"
+              >
+                <MenuItem disabled={true}>Shipping</MenuItem>
+
+                <MenuItem value="0">No</MenuItem>
+                <MenuItem value="1">Yes</MenuItem>
+              </Select>
+            </FormControl>
+            <TextField
+              variant="outlined"
+              style={{ width: "100%", marginBottom: "5%", marginTop: "5%" }}
+              value={quantity}
+              onChange={handleChange("quantity")}
+              type="number"
+              label="Quantity"
+            />
+          </Grid>
+          <Grid
+            item
+            xs
+            style={{ padding: "3%", paddingTop: "1%", paddingBottom: "1%" }}
+          >
+            {photo === "" ? (
+              <div
+                style={{
+                  width: "100%",
+                  marginBottom: "5%",
+                  marginTop: "5%",
+                }}
+              >
+                <DropzoneArea
+                  getFileRemovedMessage={(fileName) => {
+                    setValues({ ...values, photo: "" });
+                    return `File ${fileName} removed.`;
+                  }}
+                  filesLimit={1}
+                  acceptedFiles={["image/*"]}
+                  onChange={handleImage("photo")}
+                />
+              </div>
+            ) : (
+              <div style={{ position: "relative" }}>
+                <IconButton
+                  onClick={() => setValues({ ...values, photo: "" })}
+                  aria-label="delete"
+                  style={{
+                    background: "red",
+                    color: "white",
+                    position: "absolute",
+                    top: "-1%",
+                    right: "-5%",
+                    zIndex: 5,
+                  }}
+                >
+                  <DeleteIcon />
+                </IconButton>
+                <img
+                  style={{
+                    width: "100%",
+                    height: 250,
+                    marginBottom: "5%",
+                    marginTop: "5%",
+                    opacity: 1,
+                  }}
+                  src={
+                    photo.toString().includes("http")
+                      ? photo
+                      : URL.createObjectURL(photo)
+                  }
+                  alt={"Product Image"}
+                />
+              </div>
+            )}
+
+            <TextField
+              style={{ width: "100%", marginBottom: "1%", marginTop: "1%" }}
+              id="outlined-textarea"
+              label="Description"
+              placeholder="Description"
+              type="text"
+              multiline={true}
+              variant="outlined"
+              rows="4"
+              value={description}
+              onChange={handleChange("description")}
+            />
+
+            <Button
+              style={{ marginTop: "5%", width: "100%" }}
+              onClick={clickSubmit}
+              variant="contained"
+              color="primary"
+              size="large"
+            >
+              Update Product
+            </Button>
+          </Grid>
+        </Grid>
+      </Paper>
+    );
+  };
+
+  const showError = () => {
+    if (error !== "") {
+      return (
+        <Snackbar
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left",
+          }}
+          open={true}
+          autoHideDuration={5000}
+          onClose={() => setValues({ ...values, error: "" })}
+        >
+          <MuiAlert
+            onClose={() => setValues({ ...values, error: "" })}
+            variant="filled"
+            severity="error"
+          >
+            {error}
+          </MuiAlert>
+        </Snackbar>
+      );
+    }
+  };
+
+  const showSuccess = () => {
+    if (createdProduct) {
+      return (
+        <Snackbar
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left",
+          }}
+          open={true}
+          autoHideDuration={5000}
+          onClose={() => setValues({ ...values, createdProduct: "" })}
+        >
+          <MuiAlert
+            onClose={() => setValues({ ...values, createdProduct: "" })}
+            variant="filled"
+            severity="success"
+          >
+            {`${createdProduct}`} is updated!
+          </MuiAlert>
+        </Snackbar>
+      );
+    }
+  };
+
+  const showLoading = () => (
+    <Backdrop open={loading} className={classes.backdrop}>
+      <CircularProgress color="inherit" />
+    </Backdrop>
+  );
+
+  return (
+    <AdminDashboard>
+      <div className="row">
+        <div className="col-md-8 offset-md-2">
+          {showLoading()}
+          {showSuccess()}
+          {showError()}
+          {newProductForm()}
+        </div>
+      </div>
+    </AdminDashboard>
+  );
 };
 
 export default UpdateProduct;
