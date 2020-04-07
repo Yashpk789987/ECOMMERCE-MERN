0492716eefd73ff074983817b6201591cd15b5fc
yashpk789987 <yashpk789987@gmail.com>
2020-04-08 03:53:46 +0530

    List All Porducts(Admin) (Material UI)

--- frontend/src/admin/ManageProducts.js
+++ frontend/src/admin/ManageProducts.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import MaterialTable from "material-table";
+
 import AdminDashboard from "../user/AdminDashboard";
 import { isAuthenticated } from "../auth";
-import { Link } from "react-router-dom";
 import { getProducts, deleteProduct } from "./apiAdmin";
+import { API } from "../config";
 
-const ManageProducts = () => {
+const ManageProducts = ({ history }) => {
   const [products, setProducts] = useState([]);
 
   const { user, token } = isAuthenticated();
@@ -14,18 +17,23 @@ const ManageProducts = () => {
       if (data.error) {
         console.log(data.error);
       } else {
+        console.log(data);
         setProducts(data);
       }
     });
   };
 
   const destroy = (productId) => {
-    deleteProduct(productId, user._id, token).then((data) => {
-      if (data.error) {
-        console.log(data.error);
-      } else {
-        loadProducts();
-      }
+    return new Promise(function (resolve, reject) {
+      deleteProduct(productId, user._id, token).then((data) => {
+        if (data.error) {
+          console.log(data.error);
+          resolve();
+        } else {
+          loadProducts();
+          resolve();
+        }
+      });
     });
   };
 
@@ -39,7 +47,7 @@ const ManageProducts = () => {
         <div className="col-12">
           <h2 className="text-center">Total {products.length} products</h2>
           <hr />
-          <ul className="list-group">
+          {/* <ul className="list-group">
             {products.map((p, i) => (
               <li
                 key={i}
@@ -57,7 +65,40 @@ const ManageProducts = () => {
                 </span>
               </li>
             ))}
-          </ul>
+          </ul> */}
+          <MaterialTable
+            style={{ padding: "3%" }}
+            title="All Products"
+            options={{ pageSize: 3 }}
+            columns={[
+              {
+                title: "Picture",
+                field: "photo",
+                render: (p) => (
+                  <img
+                    style={{ height: 100, width: 175 }}
+                    src={`${API}/product/photo/${p._id}`}
+                  />
+                ),
+              },
+              { title: "Name", field: "name" },
+              { title: "Price", field: "price", type: "numeric" },
+              { title: "Quantity", field: "quantity", type: "numeric" },
+            ]}
+            data={products}
+            editable={{
+              onRowDelete: (p) => destroy(p._id),
+            }}
+            actions={[
+              {
+                icon: "edit",
+                tooltip: "Update",
+                onClick: (event, p) => {
+                  history.push(`/admin/product/update/${p._id}`);
+                },
+              },
+            ]}
+          />
           <br />
         </div>
       </div>
