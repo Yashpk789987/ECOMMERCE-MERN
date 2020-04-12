c147d1a2bc9c01a6f684ebd108645488a9ea402c
Yashpk789987 <yashpk789987@gmail.com>
2020-04-12 23:38:59 +0530

    Settings Tabs Material UI

--- frontend/src/core/Layout.js
+++ frontend/src/core/Layout.js
@@ -1,15 +1,3 @@
-// import React from "react";
-// import Menu from "./Menu";
-
-// const Layout = ({ children }) => (
-//   <>
-//     <Menu />
-//     <div>{children}</div>
-//   </>
-// );
-
-// export default Layout;
-
 import React from "react";
 import PropTypes from "prop-types";
 import SwipeableViews from "react-swipeable-views";
@@ -19,10 +7,18 @@ import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
 
 import Home from "./Home";
 import Signin from "../user/Signin";
 import Signup from "../user/Signup";
+
+import { signout, isAuthenticated } from "../auth";
+import { itemTotal } from "./cartHelpers";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -61,7 +57,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function FullWidthTabs() {
+export default function Layout(props) {
   const classes = useStyles();
   const theme = useTheme();
   const [value, setValue] = React.useState(0);
@@ -74,6 +70,19 @@ export default function FullWidthTabs() {
     setValue(index);
   };
 
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const tab = 0;
+
   return (
     <div className={classes.root}>
       <AppBar position="static" color="default">
@@ -85,11 +94,58 @@ export default function FullWidthTabs() {
           variant="fullWidth"
           aria-label="full width tabs example"
         >
-          <Tab label="Home" {...a11yProps(0)} />
-          <Tab label="Shop" {...a11yProps(1)} />
-          <Tab label="Cart" {...a11yProps(2)} />
-          <Tab label="Login" {...a11yProps(3)} />
-          <Tab label="Register" {...a11yProps(4)} />
+          <Tab label="Home" {...a11yProps(tab)} />
+          <Tab label="Shop" {...a11yProps(tab + 1)} />
+          <Tab label="Cart" {...a11yProps(tab + 2)} />
+          {!isAuthenticated() && [
+            <Tab label="Login" {...a11yProps(tab + 3)} />,
+            <Tab label="Register" {...a11yProps(tab + 4)} />,
+          ]}
+          {isAuthenticated() && isAuthenticated().user.role === 0 && (
+            <div>
+              <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem
+                  onClick={() => {
+                    props.history.push("/user/dashboard");
+                  }}
+                >
+                  Profile
+                </MenuItem>
+                <MenuItem
+                  onClick={() => {
+                    signout(() => {
+                      props.history.push("/");
+                    });
+                  }}
+                >
+                  Logout
+                </MenuItem>
+              </Menu>
+            </div>
+          )}
         </Tabs>
       </AppBar>
       <SwipeableViews
@@ -98,21 +154,25 @@ export default function FullWidthTabs() {
         index={value}
         onChangeIndex={handleChangeIndex}
       >
-        <TabPanel value={value} index={0} dir={theme.direction}>
+        <TabPanel value={value} index={tab} dir={theme.direction}>
           <Home />
         </TabPanel>
-        <TabPanel value={value} index={1} dir={theme.direction}>
+        <TabPanel value={value} index={tab + 1} dir={theme.direction}>
           Item Two
         </TabPanel>
-        <TabPanel value={value} index={2} dir={theme.direction}>
+        <TabPanel value={value} index={tab + 2} dir={theme.direction}>
           Item Three
         </TabPanel>
-        <TabPanel value={value} index={3} dir={theme.direction}>
-          <Signin />
-        </TabPanel>
-        <TabPanel value={value} index={4} dir={theme.direction}>
-          <Signup />
-        </TabPanel>
+        {!isAuthenticated() && (
+          <TabPanel value={value} index={tab + 3} dir={theme.direction}>
+            <Signin />
+          </TabPanel>
+        )}
+        {!isAuthenticated() && (
+          <TabPanel value={value} index={tab + 4} dir={theme.direction}>
+            <Signup />
+          </TabPanel>
+        )}
       </SwipeableViews>
     </div>
   );
